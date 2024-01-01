1520cb7c540cfca25d7d5ce1eb5925b7115b215a
Sukhrobbek Soatov <114657195+svkhrobbeck@users.noreply.github.com>
2024-01-01 17:32:52 +0500

    add changes

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const elTitle = document.getElementById("title");
 let intervalId = 0;
 
 const updateTitle = (nextYear, days, hours, minutes, seconds) => {
-  const day = days ? `${days} kun${hours ? "," : ""}` : "";
-  const hour = hours ? `${hours} soat${minutes ? "," : ""}` : "";
-  const minute = minutes ? `${minutes} daqiqa` : "";
+  const day = days ? `${days} kun` : `<span class="left">0 kun</span>`;
+  const hour = hours ? `${hours} soat` : `<span class="left">0 soat</span>`;
+  const minute = minutes
+    ? `${minutes} daqiqa`
+    : `<span class="left">0 daqiqa</span>`;
   const second = `va ${seconds} soniya`;
 
-  const str = `${nextYear}-yil kirib kelishiga ${day} ${hour} ${minute} ${second} qoldi`;
-  elTitle.textContent = str;
+  const str = `${nextYear}-yil kirib kelishiga ${day}, ${hour}, ${minute} ${second} qoldi`;
+  elTitle.innerHTML = str;
 };
 
 const startCountdown = () => {
@@ -31,7 +33,7 @@ const startCountdown = () => {
     minutes = Math.floor((residualTime / (1000 * 60)) % 60);
     seconds = Math.floor((residualTime / 1000) % 60);
 
-    if (residualTime < 1) elTitle.textContent = "yangi yil kirib keldi!";
+    if (residualTime < 1) elTitle.textContent = `${nextYear} yil kirib keldi!`;
     else updateTitle(nextYear, days, hours, minutes, seconds);
   }, 1000);
 };
