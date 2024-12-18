b46547a715ddf9e6e74695b93308e20d77efc105
svkhrobbeck <svkhrobbeck@gmail.com>
2024-12-18 11:23:47 +0500

    feat: add changes

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ let intervalId = 0;
 const updateTitle = (nextYear, days, hours, minutes, seconds) => {
   const day = days ? `${days} kun` : `<span class="left">0 kun</span>`;
   const hour = hours ? `${hours} soat` : `<span class="left">0 soat</span>`;
-  const minute = minutes
-    ? `${minutes} daqiqa`
-    : `<span class="left">0 daqiqa</span>`;
+  const minute = minutes ? `${minutes} daqiqa` : `<span class="left">0 daqiqa</span>`;
   const second = `va ${seconds} soniya`;
 
   const str = `${nextYear}-yil kirib kelishiga ${day}, ${hour}, ${minute} ${second} qoldi`;
@@ -17,8 +15,9 @@ const updateTitle = (nextYear, days, hours, minutes, seconds) => {
 };
 
 const startCountdown = () => {
-  const nextYear = new Date().getFullYear() + 1;
-  const nextYearTime = new Date(`01.01.${nextYear}`);
+  const newYear = new Date().getFullYear();
+  const nextYear = newYear + 1;
+  const newYearTime = new Date(`12.31.${newYear}`);
 
   let days = 365;
   let hours = 24;
@@ -26,7 +25,7 @@ const startCountdown = () => {
   let seconds = 60;
 
   intervalId = setInterval(() => {
-    const residualTime = nextYearTime - Date.now();
+    const residualTime = newYearTime - Date.now();
 
     days = Math.floor(residualTime / (1000 * 60 * 60 * 24));
     hours = Math.floor((residualTime / (1000 * 60 * 60)) % 24);
